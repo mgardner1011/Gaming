97477a0c066a3ee60886c1375b0de660b1ed6c2b
mgardner1011 <mgardner1011@gmail.com>
2022-04-10 18:48:49 -0500

    Create CurrentPlayerDisplay

---
+++ CurrentPlayerDisplay.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentPlayerDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        theStateManager = GameObject.FindObjectOfType<StateManager>();
+        myText = GetComponent <Text>();
+    }
+
+    Text myText;
+    StateManager theStateManager;
+
+    string[] numberWords = { "One", "Two" };
+
+    // Update is called once per frame
+    void Update()
+    {
+        myText.text = "Current Player: " + numberWords[theStateManager.CurrentPlayerId];
+    }
+}
