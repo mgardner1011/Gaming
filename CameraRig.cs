e58161b5370a656940c3e6403c05f2ed8ba7aeda
mgardner1011 <mgardner1011@gmail.com>
2022-04-11 19:21:37 -0500

    Create CameraRig

---
+++ CameraRig.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRig : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        theStateManager = GameObject.FindObjectOfType<StateManager>();
+    }
+
+    StateManager theStateManager;
+
+    public float PivotAngle = 35f;
+    float pivotVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float theAngle = this.transform.rotation.eulerAngles.y;
+        if (theAngle > 180)
+            theAngle -= 360f;
+
+        theAngle = Mathf.SmoothDamp(
+            theAngle,
+            (theStateManager.CurrentPlayerId == 0 ? PivotAngle : -PivotAngle),
+            ref pivotVelocity,
+            .25f);
+
+        this.transform.rotation = Quaternion.Euler(new Vector3(0, theAngle, 0));
+    }
+}
